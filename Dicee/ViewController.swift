d42ac581152c9b91e3b2d85d1e075d07bda0da5b
Jonathan Go <jogo88@gmail.com>
2017-08-06 11:54:27 +0900

    Optimized. DRY - dont repeat yourself

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -21,7 +21,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,12 +32,16 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        
         randomDiceIndex1 = Int(arc4random_uniform(6))  //6 is the upper bound (0 - 6)
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-        
     }
 
 }
